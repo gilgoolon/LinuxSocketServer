017c440c34bb4c95e0e944f1b2b9661fd0f501da
Gil Alpert <alpertgil@gmail.com>
2024-09-24 13:25:20 +0300

    Final cleans and proper exception handling

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,6 +1,7 @@
 #include <thread>
 
 #include "Server.h"
+#include <iostream>
 
 Server::Server(std::unique_ptr<ServerSocket> server_socket, size_t max_connections)
     : m_server_socket(std::move(server_socket)), m_max_connections(max_connections)
@@ -12,16 +13,31 @@ void Server::run() const
     m_server_socket->listen(m_max_connections);
     while (true)
     {
-        auto client_socket = m_server_socket->accept();
-        std::thread client_handler_thread(&Server::handle_client, this, std::move(client_socket));
-        client_handler_thread.detach();
+        try
+        {
+            auto client_socket = m_server_socket->accept();
+            std::thread client_handler_thread(&Server::handle_client, this, std::move(client_socket));
+            client_handler_thread.detach();
+        }
+        catch (const std::exception &ex)
+        {
+            std::cout << "Failed to start handling client. Error: " << ex.what() << std::endl;
+        }
     }
 }
 
 void Server::handle_client(const std::unique_ptr<Socket> client_socket) const
 {
-    const auto buff = client_socket->receive();
-    client_socket->send(buff);
+    try
+    {
+
+        const auto buff = client_socket->receive();
+        client_socket->send(buff);
+    }
+    catch (const std::exception &ex)
+    {
+        std::cout << "Failed to handle client. Error: " << ex.what() << std::endl;
+    }
 }
 
 std::unique_ptr<Server> create_server(const int port, const size_t max_connections)
--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,20 @@ int main(const int argc, char **argv)
     if (EXPECTED_ARGUMENTS != argc)
     {
         std::cout << "Usage: " << argv[0] << " [port]" << std::endl;
-        return 1;
+        return EXIT_FAILURE;
     }
     const int server_port = std::stoi(argv[1]);
     const size_t max_connections = 10;
     std::cout << "Running server on port: " << server_port << std::endl;
-    const auto server = create_server(server_port, max_connections);
-    server->run();
-    return 0;
+    try
+    {
+        const auto server = create_server(server_port, max_connections);
+        server->run();
+    }
+    catch (const std::exception &ex)
+    {
+        std::cout << "Failed to create/run server. Error: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
