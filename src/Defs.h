0a1ecd9bff3b1cfb30bbe4734083b8eed75260c7
Gil Alpert <alpertgil@gmail.com>
2024-09-24 12:48:41 +0300

    Implemented recv/send socket interface. Fully working server

--- src/Defs.h
+++ src/Defs.h
@@ -3,3 +3,6 @@
 
 using Byte = unsigned char;
 using Buffer = std::vector<Byte>;
+
+constexpr int UNIX_INT_ERROR_VALUE = -1;
+constexpr size_t DEFAULT_BUFF_SIZE = 4096;
--- src/common/Socket.cpp
+++ src/common/Socket.cpp
@@ -1,6 +1,8 @@
-#include "Socket.h"
+#include <sys/socket.h>
 #include <unistd.h>
 
+#include "Socket.h"
+#include "Utils.h"
 
 Socket::Socket(int socket_fd)
     : m_socket_fd(socket_fd)
@@ -14,9 +16,24 @@ Socket::~Socket()
 
 void Socket::send(const Buffer &data) const
 {
+    covered_call(UNIX_INT_ERROR_VALUE, ::send, m_socket_fd, data.data(), data.size(), 0);
 }
 
 Buffer Socket::receive() const
 {
-    return Buffer();
+    Buffer buff;
+    while (true)
+    {
+        const size_t buff_size = DEFAULT_BUFF_SIZE;
+        const size_t old_size = buff.size();
+        buff.resize(old_size + buff_size);
+
+        const ssize_t bytes_read = covered_call(UNIX_INT_ERROR_VALUE, ::recv, m_socket_fd, buff.data() + old_size, buff_size, 0);
+        buff.resize(old_size + bytes_read);
+
+        if (bytes_read < buff_size) {
+            break;
+        }
+    }
+    return buff;
 }
