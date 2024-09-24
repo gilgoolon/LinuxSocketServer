d8964ca3cc87fa7b643c33c725c1c4a277eee85a
Gil Alpert <alpertgil@gmail.com>
2024-09-24 17:52:03 +0300

    Clean and substitute magics

--- src/Defs.hpp
+++ src/Defs.hpp
@@ -7,3 +7,5 @@ using Buffer = std::vector<Byte>;
 
 constexpr int UNIX_INT_ERROR_VALUE = -1;
 constexpr size_t DEFAULT_BUFF_SIZE = 4096;
+constexpr int DEFAULT_NO_FLAGS = 0;
+constexpr void *OPTIONAL_NO_OUTPUT = nullptr;
--- src/common/ServerSocket.cpp
+++ src/common/ServerSocket.cpp
@@ -18,22 +18,22 @@ ServerSocket::~ServerSocket()
 
 void ServerSocket::bind(const sockaddr_in &server_address) const
 {
-    covered_call(-1, ::bind, m_socket_fd, reinterpret_cast<const sockaddr *>(&server_address), sizeof(server_address));
+    covered_call(UNIX_INT_ERROR_VALUE, ::bind, m_socket_fd, reinterpret_cast<const sockaddr *>(&server_address), sizeof(server_address));
 }
 
 void ServerSocket::listen(size_t max_connections) const
 {
-    covered_call(-1, ::listen, m_socket_fd, max_connections);
+    covered_call(UNIX_INT_ERROR_VALUE, ::listen, m_socket_fd, max_connections);
 }
 
 std::unique_ptr<Socket> ServerSocket::accept() const
 {
-    return std::make_unique<Socket>(covered_call(-1, ::accept, m_socket_fd, nullptr, nullptr));
+    return std::make_unique<Socket>(covered_call(UNIX_INT_ERROR_VALUE, ::accept, m_socket_fd, OPTIONAL_NO_OUTPUT, OPTIONAL_NO_OUTPUT));
 }
 
 std::unique_ptr<ServerSocket> create_server_socket(int port)
 {
-    auto server_socket = std::make_unique<ServerSocket>(covered_call(-1, &::socket, AF_INET, SOCK_STREAM, 0));
+    auto server_socket = std::make_unique<ServerSocket>(covered_call(UNIX_INT_ERROR_VALUE, &::socket, AF_INET, SOCK_STREAM, DEFAULT_NO_FLAGS));
     sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = ::htons(port);
